8eeb9e45f5026a8c477156516159ef1dcc65b54d
blurringmyday <amandadunne03@gmail.com>
2015-09-24 16:02:37 -0500

    added buttons, variable for container size

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 	
 	
+	var n = prompt("Please enter a value between 1 and 100"); 
 
-	for(var i = 0; i < 10; i++){
-		for(var j = 0; j < 10; j++){
-		$('<div />').addClass('grid').appendTo('#container');	
+	for(var i = 0; i < n; i++){
+		for(var j = 0; j < n; j++){
+		$('<div/>').addClass('grid').appendTo('#container');	
 		}
 		console.log("done!");
 	}
@@ -12,10 +13,14 @@ $(document).ready(function(){
 	$('.grid').hover(
 		function(){
 		$(this).addClass('active');
-		}, function(){
-			$(this).removeClass('active');
 		});
 
+	$('#container').height(n*10); 
+	$('#container').width(n*10); 
+
+	$('#clear').click(function(){
+		$('.grid').removeClass('active');
+	})
 
 });
 
